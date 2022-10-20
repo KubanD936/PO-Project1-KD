2d4511582b3ab7c6bd55d121d3a044aa69ab7a3b
KubanD936 <kuban627@revature.net>
2022-10-20 15:46:46 -0400

    Fourth Commit
    Added ReimbursementService interface and Users

---
+++ src/main/java/dev/kuband/entities/Users.java
@@ -0,0 +1,109 @@
+package dev.kuband.entities;
+
+import java.util.Objects;
+
+public class Users {
+    //Users parameters
+    private int user_id;
+    private String username;
+    private String password;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String role;
+
+    public Users() {
+    }
+
+    public Users(int user_id, String username, String password, String first_name, String last_name, String email, String role) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "user_id=" + user_id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return user_id == users.user_id && Objects.equals(username, users.username) && Objects.equals(password, users.password) && Objects.equals(first_name, users.first_name) && Objects.equals(last_name, users.last_name) && Objects.equals(email, users.email) && Objects.equals(role, users.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user_id, username, password, first_name, last_name, email, role);
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/dev/kuband/services/ReimbursementService.java
@@ -0,0 +1,25 @@
+package dev.kuband.services;
+
+import dev.kuband.entities.Reimbursement;
+
+import java.util.List;
+
+public interface ReimbursementService {
+    //Create reimbursement
+    Reimbursement createReimbursement(Reimbursement reimbursement);
+
+    //Retrieve reimbursement
+    Reimbursement getReimbursementById(int id);
+
+    //Retrieve all reimbursements
+    List<Reimbursement> getAllReimbursement();
+
+    //Retrieve reimbursement based on user
+    //List<Reimbursement> getReimbursementByUser(User user);
+
+    //Update reimbursement
+    Reimbursement updateReimbursement(Reimbursement reimbursement);
+
+    //Delete reimbursement
+    boolean deleteReimbursementById(int id);
+}
